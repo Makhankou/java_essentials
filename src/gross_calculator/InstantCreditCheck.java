75367b2f5da2e3c7bf87af258393a492505c640b
Makhankou <84236629+Makhankou@users.noreply.github.com>
2026-06-11 00:06:08 +0300

    Add files via upload

---
+++ src/gross_calculator/InstantCreditCheck.java
@@ -0,0 +1,36 @@
+package gross_calculator;
+
+import java.util.Scanner;
+
+public class InstantCreditCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your salary: ");
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter your credit score: ");
+        int creditScore = scanner.nextInt();
+        scanner.close();
+        boolean qualified = isUserQualified(creditScore, salary);
+        notifyUser(qualified);
+    }
+
+    public static boolean isUserQualified(int creditScore, double salary) {
+        double requiredSalary = 25000;
+        int requiredCreditScore = 700;
+
+        if(creditScore >= requiredCreditScore && salary >= requiredSalary) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void notifyUser(boolean isQualified) {
+        if(isQualified) {
+            System.out.println("Congrats! You've been approved!");
+        } else {
+            System.out.println("Sorry! You've been declined!");
+        }
+    }
+}
